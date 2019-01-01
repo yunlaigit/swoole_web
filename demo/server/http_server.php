084d9d871c44f0a32754e4418467d75295f862f0
yunlai <826132167@qq.com>
2019-01-01 15:26:33 +0800

    http server下

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -1,8 +1,15 @@
 <?php
     $http = new swoole_http_server("0.0.0.0", 8811);
 
+    $http->set([
+        'enable_static_handler' => true,
+        'document_root' => '/home/git/swoole_web/data',
+    ]);
+
     $http->on('request', function ($request, $response) {
-        $response->end("<h1>HTTPSERVERS</h1>");
+//        print_r($request->get);
+        $response->cookie("test", "test_val", time() + 1800);
+        $response->end("sss".json_encode($request->get));
     });
 
     $http->start();
